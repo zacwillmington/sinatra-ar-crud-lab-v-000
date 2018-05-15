aaa8ec6c7b9fcf446a56d8ddc9f99bf5531164ba
Zac Willmington <zac.willmington@gmail.com>
2018-05-15 03:09:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
       erb :edit
   end
 
-  patch '/posts/:id/edit' do
+  patch '/posts/:id' do
       @post = Post.find(params[:id])
       @post.update('name' => params[:name], 'content' => params[:content])
       erb :edit
